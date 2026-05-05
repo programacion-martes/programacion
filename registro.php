9416d4b5f18640dd66696e65f67d601ae058e7d2
Efereank <franciscojavier20006@gmail.com>
2026-05-05 04:51:26 -0400

    cambio_final

--- registro.php
+++ registro.php
@@ -18,17 +18,21 @@ $token = TokenAntiCSRF::generarToken();
     <link rel="stylesheet" href="assets/css/app.css">
 </head>
 <body>
-    <h2>Registrar Usuario</h2>
     
     <form action="procesar.php" method="post">
-        <input type="hidden" name="token" value="<?php echo $token; ?>">
+        
+    <h2>Registrar Usuario</h2>
+    
+    <input type="hidden" name="token" value="<?php echo $token; ?>">
         <label>Usuario:</label><br>
         <input type="text" name="usuario" required><br><br>
         <label>Contraseña:</label><br>
         <input type="password" name="contraseña" required><br><br>
         <button type="submit" name="enviar">Registrar</button>
+        
+        <a href="index.php">¿Ya tienes cuenta? Inicia sesión aquí</a>
     </form>
+    
     <br>
-    <a href="index.php">¿Ya tienes cuenta? Inicia sesión aquí</a>
 </body>
 </html>
